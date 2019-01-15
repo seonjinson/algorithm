609cde4b0bd6b82e0d535c7650910bdf87d46c3c
seonjinson <akqmfspt07@naver.com>
2019-01-15 11:09:54 +0900

    K번째 수

--- src/ValueOfKNumber.java
+++ src/ValueOfKNumber.java
@@ -15,17 +15,17 @@ public class ValueOfKNumber {
     public int[] solution (int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         ArrayList<Integer> arrlist= null;
-        System.out.println(commands.length);
+        //System.out.println(commands.length);
 
         for(int i=0; i<commands.length; i++){
             arrlist = new ArrayList<>();
-            System.out.println("commands[i][1]"+commands[i][1]);
+            //System.out.println("commands[i][1]"+commands[i][1]);
             for(int j=commands[i][0]; j<=commands[i][1];j++) {
                 arrlist.add(array[j-1]);
-                System.out.println("j "+array[j-1]);
+                //System.out.println("j "+array[j-1]);
             }
             Collections.sort(arrlist);
-            answer[i] = arrlist.get(commands[i][2]);
+            answer[i] = arrlist.get(commands[i][2]-1);
 
         }
         for(int i=0; i<answer.length;i++){
